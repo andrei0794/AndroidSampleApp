f4ab0b87851d2ee1a47f930a6f09e55cced2e758
Andrei Ioan <andrei.0794@yahoo.com>
2018-01-30 22:45:20 +0200

    Added UIscripts

---
+++ Assets/Scripts/GUI/UIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    private Text scoreCounter;
+    private void Start()
+    {
+        scoreCounter = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update () {
+		if (Globals._tileCounter < 10)
+        {
+            scoreCounter.text = "0" + Globals._tileCounter;
+        }
+        else
+        {
+            scoreCounter.text = Globals._tileCounter.ToString();
+        }
+	}
+}
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Globals : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static int _tileCounter;
 }
